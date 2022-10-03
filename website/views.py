877c75ab7d98cdd820e70f1372d954b622f7dbf1
JurgisGit <lt.jurgis@gmail.com>
2022-10-03 21:33:58 +0300

    new routese

--- website/views.py
+++ website/views.py
@@ -4,7 +4,15 @@ from django.template import loader
 
 def index(request):
     template = loader.get_template('index.html')
-    context = {
+    context = {}
+    return HttpResponse(template.render(context, request))
 
-    }
+def login(request):
+    template = loader.get_template('login.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def signup(request):
+    template = loader.get_template('signup.html')
+    context = {}
     return HttpResponse(template.render(context, request))
