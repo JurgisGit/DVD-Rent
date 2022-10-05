2cc68f46087be138d5ad9673ba6a417cd9aaddc2
JurgisGit <lt.jurgis@gmail.com>
2022-10-05 20:59:49 +0300

    pirma lentele, duomenu baze

--- website/models.py
+++ website/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Film(models.Model):
+    title = models.CharField('Title', max_length=200)
+    director = models.CharField('Director', max_length=200)
+    year = models.IntegerField('Year')
--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Film
 
 
 def index(request):
     template = loader.get_template('index.html')
-    context = {}
+    films = Film.objects.all()
+    context = {'films': films}
     return HttpResponse(template.render(context, request))
 
 def login(request):
@@ -15,4 +17,12 @@ def login(request):
 def signup(request):
     template = loader.get_template('signup.html')
     context = {}
+    if request.method == "POST":
+        # pasiimame reikšmes iš registracijos formos
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        print(username, email, password, password2, request.method)
     return HttpResponse(template.render(context, request))
+
