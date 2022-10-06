61927f2c1e093e71a659a4717cd69be9cf0eada3
JurgisGit <lt.jurgis@gmail.com>
2022-10-06 20:45:27 +0300

    registration, login

--- website/views.py
+++ website/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.template import loader
 from .models import Film, Director
+from django.contrib.auth.forms import User
+from django.contrib.auth import logout, authenticate, login
 
 
 def index(request):
@@ -8,14 +11,22 @@ def index(request):
     context = {'films': Film.objects.all()}
     return HttpResponse(template.render(context, request))
 
-def login(request):
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+
     template = loader.get_template('login.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
+
 def signup(request):
-    template = loader.get_template('signup.html')
-    context = {}
     if request.method == "POST":
         # pasiimame reikšmes iš registracijos formos
         username = request.POST['username']
@@ -23,9 +34,20 @@ def signup(request):
         password = request.POST['password']
         password2 = request.POST['password2']
         print(username, email, password, password2, request.method)
-    return HttpResponse(template.render(context, request))
+        User.objects.create_user(username, email, password)
+        return redirect('index')
+    elif request.method == "GET":
+        template = loader.get_template('signup.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+
 
 def directors(request):
     template = loader.get_template('directors.html')
     context = {'directors': Director.objects.all()}
     return HttpResponse(template.render(context, request))
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
